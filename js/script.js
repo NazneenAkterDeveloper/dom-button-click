3b348cf858e7e62611339331306be548fe67c5a7
Nazneen Akter <nazneenzaman195@gmail.com>
2025-05-22 11:36:17 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,56 @@
+// let heading = document.getElementsByTagName("h1")
+// console.log(heading[1].innerHTML)
+
+// let heading = document.getElementsByClassName("heading")
+// console.log(heading.innerHTML)
+
+// let heading = document.getElementById("heading")
+// console.log(heading.innerHTML)
+
+// let heading = document.querySelector("h1")
+// console.log(heading.innerHTML)
+
+// let heading = document.querySelector("h1")
+// let button = document.querySelector("button")
+
+// button.addEventListener("click",()=>{
+//     heading.innerHTML = "Hello World"
+//     heading.style.color = "red"
+//     heading.style.fontSize = "50px"
+//     heading.style.textAlign = "center"
+//     heading.style.backgroundColor = "black"
+//     heading.style.padding = "20px"
+//     heading.style.borderRadius = "10px"
+
+// })
+let nameInput = document.querySelector(".nameInput")
+let clickme = document.querySelector(".clickme")
+let paragraph = document.querySelector(".paragraph")
+
+   
+    
+
+clickme.addEventListener("click",()=>{
+
+nameInput.value=("h1")
+   
+    // paragraph.innerHTML = "Hello World"
+    // paragraph.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus."
+    // paragraph.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus."
+
+
+
+//    paragraph.innerHTML = "Lorem ipsum, dolor sit amet consectetur adipisicing elit!"
+
+
+
+
+   
+//     paragraph.style.color = "navy"
+//   paragraph.style.fontSize = "30px"
+//   paragraph.style.textAlign = "center"
+//   paragraph.style.backgroundColor = "skyblue"
+//     paragraph.style.padding = "20px"
+//   paragraph.style.borderRadius = "10px"
+//     paragraph.style.width = "100%"
+})
